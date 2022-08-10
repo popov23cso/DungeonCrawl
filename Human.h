8586156580f0396f9950602f49d7dd4c0d3f689e
popov23cso <popov23cso@gmail.com>
2022-08-10 19:52:46 +0300

    Update Human.h
    Defined human class

--- Human.h
+++ Human.h
@@ -1,1 +1,32 @@
-//TODO add a human class
+#pragma once
+#include "Entity.h"
+#include "EntityInterface.h"
+
+class Human : public Entity
+{
+	int weapon;
+	int armor;
+	std::string name;
+	int pouch;
+public:
+	//constructors and destructors
+	Human();
+	Human(int weapon, int armor, char* name, int health);
+	~Human() override;
+
+	//text output methods
+	void PrintType() const override;
+	void Print()  const override;
+
+	//getters and setters
+	int GetType() override;
+	void AddCoins(int Amount) override;
+	int GetCoins() override;
+
+
+
+	//gameplay methods
+	int Attack() override;
+	int Defend(int DmgTaken) override;
+	void UseItem(int ItemID, int Effect) override;
+};
